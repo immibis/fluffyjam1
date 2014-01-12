86698d740b2132483663b000e441508b0019ccfd
immibis <immibis@gmail.com>
2014-01-12 15:30:50 +1300

    Initial commit with empty mod.

---
+++ com/immibis/fluffyjam1/FluffyJam1Mod.java
@@ -0,0 +1,10 @@
+package com.immibis.fluffyjam1;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid="immibis_fj1", name="Immibis Fluffy Jam 1 Untitled Mod", version="1.0")
+@NetworkMod(clientSideRequired=true, serverSideRequired=false)
+public class FluffyJam1Mod {
+	
+}
